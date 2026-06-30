80410bf5c31aeb517133f4e28a2253091b072745
Morten Skriver <42451970+moskrive@users.noreply.github.com>
2026-06-30 11:45:37 +0200

    add intersight syslog (#5)

--- intersight_server_profile_templates.tf
+++ intersight_server_profile_templates.tf
@@ -13,6 +13,7 @@ locals {
         imc_access_policy_key       = try(tmpl.imc_access_policy, null) != null ? format("%s/%s", org.name, tmpl.imc_access_policy) : null
         lan_connectivity_policy_key = try(tmpl.lan_connectivity_policy, null) != null ? format("%s/%s", org.name, tmpl.lan_connectivity_policy) : null
         ntp_policy_key              = try(tmpl.ntp_policy, null) != null ? format("%s/%s", org.name, tmpl.ntp_policy) : null
+        syslog_policy_key           = try(tmpl.syslog_policy, null) != null ? format("%s/%s", org.name, tmpl.syslog_policy) : null
         virtual_kvm_policy_key      = try(tmpl.virtual_kvm_policy, null) != null ? format("%s/%s", org.name, tmpl.virtual_kvm_policy) : null
         uuid_pool_key               = try(tmpl.uuid_pool, null) != null ? format("%s/%s", org.name, tmpl.uuid_pool) : null
       }] : []
@@ -66,6 +67,14 @@ resource "intersight_server_profile_template" "server_profile_template" {
     content {
       object_type = "ntp.Policy"
       moid        = intersight_ntp_policy.ntp_policy[each.value.ntp_policy_key].moid
+    }
+  }
+
+  dynamic "policy_bucket" {
+    for_each = each.value.syslog_policy_key != null ? [1] : []
+    content {
+      object_type = "syslog.Policy"
+      moid        = intersight_syslog_policy.syslog_policy[each.value.syslog_policy_key].moid
     }
   }
 

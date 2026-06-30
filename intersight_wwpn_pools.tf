ef9cf58f746fa639ed925d443f963de9189d1d4a
Morten Skriver <moskrive@cisco.com>
2026-06-30 12:42:31 +0200

    add intersight wwpn pool

---
+++ intersight_wwpn_pools.tf
@@ -0,0 +1,39 @@
+locals {
+  wwpn_pools = flatten([
+    for org in try(local.intersight.organizations, []) : [
+      for pool in try(org.pools.wwpn, []) :
+      try(pool.managed, true) ? [merge(
+        local.defaults.compute.intersight.organizations.pools.wwpn,
+        pool,
+        {
+          key      = format("%s/%s", org.name, pool.name)
+          org_name = org.name
+          name     = pool.name
+        }
+      )] : []
+    ]
+  ])
+}
+
+resource "intersight_fcpool_pool" "wwpn_pool" {
+  for_each = { for p in local.wwpn_pools : p.key => p }
+
+  name         = each.value.name
+  description  = try(each.value.description, "")
+  pool_purpose = "WWPN"
+
+  dynamic "id_blocks" {
+    for_each = try(each.value.wwpn_blocks, [])
+    content {
+      object_type = "fcpool.Block"
+      from        = id_blocks.value.from
+      size        = try(id_blocks.value.size, null)
+      to          = try(id_blocks.value.to, null)
+    }
+  }
+
+  organization {
+    object_type = "organization.Organization"
+    moid        = local.org_moids[each.value.org_name]
+  }
+}

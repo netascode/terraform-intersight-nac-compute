a4ecf6f40ed87086310d3dc56fa70a0d55dbc461
Morten Skriver <moskrive@cisco.com>
2026-06-30 14:18:05 +0200

    flatten datamodel

--- intersight_fibre_channel_adapter_policies.tf
+++ intersight_fibre_channel_adapter_policies.tf
@@ -12,13 +12,15 @@ locals {
         lun_count                   = try(policy.lun_count, local.defaults.compute.intersight.organizations.policies.fibre_channel_adapter.lun_count)
         lun_queue_depth             = try(policy.lun_queue_depth, local.defaults.compute.intersight.organizations.policies.fibre_channel_adapter.lun_queue_depth)
         resource_allocation_timeout = try(policy.resource_allocation_timeout, local.defaults.compute.intersight.organizations.policies.fibre_channel_adapter.resource_allocation_timeout)
+        interrupt_mode              = try(policy.interrupt_mode, null)
+        flogi_retries               = try(policy.flogi_retries, null)
+        flogi_timeout               = try(policy.flogi_timeout, null)
+        plogi_retries               = try(policy.plogi_retries, null)
+        plogi_timeout               = try(policy.plogi_timeout, null)
+        rx_queue_ring_size          = try(policy.rx_queue_ring_size, null)
+        scsi_queue_ring_size        = try(policy.scsi_queue_ring_size, null)
+        tx_queue_ring_size          = try(policy.tx_queue_ring_size, null)
         error_recovery_settings     = try(policy.error_recovery_settings, null)
-        flogi_settings              = try(policy.flogi_settings, null)
-        interrupt_settings          = try(policy.interrupt_settings, null)
-        plogi_settings              = try(policy.plogi_settings, null)
-        rx_queue_settings           = try(policy.rx_queue_settings, null)
-        scsi_queue_settings         = try(policy.scsi_queue_settings, null)
-        tx_queue_settings           = try(policy.tx_queue_settings, null)
       }] : []
     ]
   ])
@@ -48,52 +50,52 @@ resource "intersight_vnic_fc_adapter_policy" "fibre_channel_adapter_policy" {
   }
 
   dynamic "flogi_settings" {
-    for_each = each.value.flogi_settings != null ? [each.value.flogi_settings] : []
+    for_each = (each.value.flogi_retries != null || each.value.flogi_timeout != null) ? [1] : []
     content {
       object_type = "vnic.FlogiSettings"
-      retries     = try(flogi_settings.value.retries, null)
-      timeout     = try(flogi_settings.value.timeout, null)
+      retries     = each.value.flogi_retries
+      timeout     = each.value.flogi_timeout
     }
   }
 
   dynamic "interrupt_settings" {
-    for_each = each.value.interrupt_settings != null ? [each.value.interrupt_settings] : []
+    for_each = each.value.interrupt_mode != null ? [1] : []
     content {
       object_type = "vnic.FcInterruptSettings"
-      mode        = try(interrupt_settings.value.mode, null)
+      mode        = each.value.interrupt_mode
     }
   }
 
   dynamic "plogi_settings" {
-    for_each = each.value.plogi_settings != null ? [each.value.plogi_settings] : []
+    for_each = (each.value.plogi_retries != null || each.value.plogi_timeout != null) ? [1] : []
     content {
       object_type = "vnic.PlogiSettings"
-      retries     = try(plogi_settings.value.retries, null)
-      timeout     = try(plogi_settings.value.timeout, null)
+      retries     = each.value.plogi_retries
+      timeout     = each.value.plogi_timeout
     }
   }
 
   dynamic "rx_queue_settings" {
-    for_each = each.value.rx_queue_settings != null ? [each.value.rx_queue_settings] : []
+    for_each = each.value.rx_queue_ring_size != null ? [1] : []
     content {
       object_type = "vnic.FcQueueSettings"
-      ring_size   = try(rx_queue_settings.value.ring_size, null)
+      ring_size   = each.value.rx_queue_ring_size
     }
   }
 
   dynamic "scsi_queue_settings" {
-    for_each = each.value.scsi_queue_settings != null ? [each.value.scsi_queue_settings] : []
+    for_each = each.value.scsi_queue_ring_size != null ? [1] : []
     content {
       object_type = "vnic.ScsiQueueSettings"
-      ring_size   = try(scsi_queue_settings.value.ring_size, null)
+      ring_size   = each.value.scsi_queue_ring_size
     }
   }
 
   dynamic "tx_queue_settings" {
-    for_each = each.value.tx_queue_settings != null ? [each.value.tx_queue_settings] : []
+    for_each = each.value.tx_queue_ring_size != null ? [1] : []
     content {
       object_type = "vnic.FcQueueSettings"
-      ring_size   = try(tx_queue_settings.value.ring_size, null)
+      ring_size   = each.value.tx_queue_ring_size
     }
   }
 

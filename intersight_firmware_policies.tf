988844be31e982a07e50913e1829e7dfcb5d67f1
Morten Skriver <42451970+moskrive@users.noreply.github.com>
2026-07-01 17:12:15 +0200

    add intersight firmware policy (#13)

---
+++ intersight_firmware_policies.tf
@@ -0,0 +1,39 @@
+locals {
+  firmware_policies = flatten([
+    for org in try(local.intersight.organizations, []) : [
+      for policy in try(org.policies.firmware, []) :
+      try(policy.managed, true) ? [{
+        key                    = format("%s/%s", org.name, policy.name)
+        org_name               = org.name
+        name                   = policy.name
+        description            = try(policy.description, local.defaults.compute.intersight.organizations.policies.firmware.description, "")
+        target_platform        = try(policy.target_platform, local.defaults.compute.intersight.organizations.policies.firmware.target_platform)
+        exclude_component_list = try(policy.exclude_component_list, [])
+        model_bundle_combo     = try(policy.model_bundle_combo, [])
+      }] : []
+    ]
+  ])
+}
+
+resource "intersight_firmware_policy" "firmware_policy" {
+  for_each = { for p in local.firmware_policies : p.key => p }
+
+  name                   = each.value.name
+  description            = each.value.description
+  target_platform        = each.value.target_platform
+  exclude_component_list = each.value.exclude_component_list
+
+  dynamic "model_bundle_combo" {
+    for_each = each.value.model_bundle_combo
+    content {
+      object_type    = "firmware.ModelBundleVersion"
+      model_family   = model_bundle_combo.value.model_family
+      bundle_version = model_bundle_combo.value.bundle_version
+    }
+  }
+
+  organization {
+    object_type = "organization.Organization"
+    moid        = local.org_moids[each.value.org_name]
+  }
+}

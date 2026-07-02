8f4e33e646a8e2e6fe5ef44b2c7240e98c4c6064
Morten Skriver <42451970+moskrive@users.noreply.github.com>
2026-07-02 16:22:59 +0200

    adding intersight smtp policy (#21)

---
+++ intersight_smtp_policies.tf
@@ -0,0 +1,43 @@
+locals {
+  smtp_policies = flatten([
+    for org in try(local.intersight.organizations, []) : [
+      for policy in try(org.policies.smtp, []) :
+      try(policy.managed, true) ? [merge(
+        local.defaults.compute.intersight.organizations.policies.smtp,
+        policy,
+        {
+          key      = format("%s/%s", org.name, policy.name)
+          org_name = org.name
+          name     = policy.name
+        }
+      )] : []
+    ]
+  ])
+}
+
+resource "intersight_smtp_policy" "smtp_policy" {
+  for_each = { for p in local.smtp_policies : p.key => p }
+
+  name            = each.value.name
+  description     = try(each.value.description, "")
+  enabled         = each.value.enabled
+  smtp_server     = try(each.value.smtp_server, null)
+  smtp_port       = each.value.smtp_port
+  sender_email    = try(each.value.sender_email, null)
+  smtp_recipients = try(each.value.recipients, [])
+  min_severity    = each.value.min_severity
+  enable_tls      = each.value.tls
+
+  dynamic "tags" {
+    for_each = try(each.value.tags, [])
+    content {
+      key   = tags.value.key
+      value = tags.value.value
+    }
+  }
+
+  organization {
+    object_type = "organization.Organization"
+    moid        = local.org_moids[each.value.org_name]
+  }
+}

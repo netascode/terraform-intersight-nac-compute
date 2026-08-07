795ad1cafa73acfc9ca8005933e8b0938ebe6a25
Morten Skriver <42451970+moskrive@users.noreply.github.com>
2026-08-07 10:25:50 +0200

    add intersight vsan policy (#25)
    * add vsan policy

---
+++ intersight_vsan_policies.tf
@@ -0,0 +1,66 @@
+locals {
+  vsan_policies = flatten([
+    for org in try(local.intersight.organizations, []) : [
+      for policy in try(org.policies.vsan, []) :
+      try(policy.managed, true) ? [{
+        key             = format("%s/%s", org.name, policy.name)
+        org_name        = org.name
+        name            = policy.name
+        description     = try(policy.description, local.defaults.compute.intersight.organizations.policies.vsan.description, "")
+        tags            = try(policy.tags, [])
+        uplink_trunking = try(policy.uplink_trunking, local.defaults.compute.intersight.organizations.policies.vsan.uplink_trunking)
+        vsans           = try(policy.vsans, [])
+      }] : []
+    ]
+  ])
+
+  vsan_policies_map = { for p in local.vsan_policies : p.key => p }
+
+  vsans = flatten([
+    for policy in local.vsan_policies : [
+      for vsan in policy.vsans : {
+        key        = format("%s/%s", policy.key, vsan.name)
+        policy_key = policy.key
+        name       = vsan.name
+        vsan_id    = vsan.vsan_id
+        fcoe_vlan  = try(vsan.fcoe_vlan_id, vsan.vsan_id)
+        vsan_scope = try(vsan.vsan_scope, local.defaults.compute.intersight.organizations.policies.vsan.vsans.vsan_scope)
+      }
+    ]
+  ])
+}
+
+resource "intersight_fabric_fc_network_policy" "vsan_policy" {
+  for_each = local.vsan_policies_map
+
+  name            = each.value.name
+  description     = each.value.description
+  enable_trunking = each.value.uplink_trunking
+
+  dynamic "tags" {
+    for_each = each.value.tags
+    content {
+      key   = tags.value.key
+      value = tags.value.value
+    }
+  }
+
+  organization {
+    object_type = "organization.Organization"
+    moid        = local.org_moids[each.value.org_name]
+  }
+}
+
+resource "intersight_fabric_vsan" "vsan" {
+  for_each = { for v in local.vsans : v.key => v }
+
+  name       = each.value.name
+  vsan_id    = each.value.vsan_id
+  fcoe_vlan  = each.value.fcoe_vlan
+  vsan_scope = each.value.vsan_scope
+
+  fc_network_policy {
+    object_type = "fabric.FcNetworkPolicy"
+    moid        = intersight_fabric_fc_network_policy.vsan_policy[each.value.policy_key].moid
+  }
+}

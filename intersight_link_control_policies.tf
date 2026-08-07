1f356646a877d775c63f830180bc8166a5ba8d29
Morten Skriver <moskrive@cisco.com>
2026-08-07 13:41:15 +0200

    add intersight link_control policy

---
+++ intersight_link_control_policies.tf
@@ -0,0 +1,42 @@
+locals {
+  link_control_policies = flatten([
+    for org in try(local.intersight.organizations, []) : [
+      for policy in try(org.policies.link_control, []) :
+      try(policy.managed, true) ? [merge(
+        local.defaults.compute.intersight.organizations.policies.link_control,
+        policy,
+        {
+          key      = format("%s/%s", org.name, policy.name)
+          org_name = org.name
+          name     = policy.name
+        }
+      )] : []
+    ]
+  ])
+}
+
+resource "intersight_fabric_link_control_policy" "link_control_policy" {
+  for_each = { for p in local.link_control_policies : p.key => p }
+
+  name        = each.value.name
+  description = try(each.value.description, "")
+
+  udld_settings {
+    object_type = "fabric.UdldSettings"
+    admin_state = each.value.admin_state
+    mode        = each.value.mode
+  }
+
+  dynamic "tags" {
+    for_each = try(each.value.tags, [])
+    content {
+      key   = tags.value.key
+      value = tags.value.value
+    }
+  }
+
+  organization {
+    object_type = "organization.Organization"
+    moid        = local.org_moids[each.value.org_name]
+  }
+}

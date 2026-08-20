2c049ba73311a38c42fe90c09eae9ce9d3ebf430
Morten Skriver <moskrive@cisco.com>
2026-08-20 10:04:52 +0200

    correct resource pool resource_type attribute was corrected (the provider doesn't support server_type inside resource_pool_parameters) and related attributes in schema

--- intersight_resource_pools.tf
+++ intersight_resource_pools.tf
@@ -3,13 +3,13 @@ locals {
     for org in local.filtered_intersight_organizations : [
       for pool in try(org.pools.resource, []) :
       try(pool.managed, true) ? [{
-        key         = format("%s/%s", org.name, pool.name)
-        org_name    = org.name
-        name        = pool.name
-        description = try(pool.description, local.defaults.compute.intersight.organizations.pools.resource.description, "")
-        server_type = try(pool.server_type, local.defaults.compute.intersight.organizations.pools.resource.server_type)
-        selectors   = try(pool.selectors, [])
-        tags        = try(pool.tags, [])
+        key           = format("%s/%s", org.name, pool.name)
+        org_name      = org.name
+        name          = pool.name
+        description   = try(pool.description, local.defaults.compute.intersight.organizations.pools.resource.description, "")
+        resource_type = try(pool.resource_type, local.defaults.compute.intersight.organizations.pools.resource.resource_type)
+        selectors     = try(pool.selectors, [])
+        tags          = try(pool.tags, [])
       }] : []
     ]
   ])
@@ -18,14 +18,10 @@ locals {
 resource "intersight_resourcepool_pool" "resource_pool" {
   for_each = { for p in local.resource_pools : p.key => p if var.manage_intersight_pools }
 
-  description = each.value.description
-  name        = each.value.name
-  pool_type   = "Server"
-
-  resource_pool_parameters {
-    object_type = "resourcepool.ServerPool"
-    server_type = each.value.server_type
-  }
+  description   = each.value.description
+  name          = each.value.name
+  pool_type     = "Server"
+  resource_type = each.value.resource_type
 
   dynamic "selectors" {
     for_each = each.value.selectors

00f476fb9f4b611772a118101749fa80accbb60a
Morten Skriver <42451970+moskrive@users.noreply.github.com>
2026-08-20 12:40:05 +0200

    add intersight iqn pool (#43)

---
+++ intersight_iqn_pools.tf
@@ -0,0 +1,48 @@
+locals {
+  iqn_pools = flatten([
+    for org in local.filtered_intersight_organizations : [
+      for pool in try(org.pools.iqn, []) :
+      try(pool.managed, true) ? [merge(
+        local.defaults.compute.intersight.organizations.pools.iqn,
+        pool,
+        {
+          key      = format("%s/%s", org.name, pool.name)
+          org_name = org.name
+          name     = pool.name
+        }
+      )] : []
+    ]
+  ])
+}
+
+resource "intersight_iqnpool_pool" "iqn_pool" {
+  for_each = { for p in local.iqn_pools : p.key => p if var.manage_intersight_pools }
+
+  name        = each.value.name
+  description = try(each.value.description, "")
+  prefix      = try(each.value.prefix, null)
+
+  dynamic "iqn_suffix_blocks" {
+    for_each = try(each.value.iqn_suffix_blocks, [])
+    content {
+      object_type = "iqnpool.IqnSuffixBlock"
+      suffix      = try(iqn_suffix_blocks.value.suffix, null)
+      from        = try(iqn_suffix_blocks.value.from, null)
+      size        = try(iqn_suffix_blocks.value.size, null)
+      to          = try(iqn_suffix_blocks.value.to, null)
+    }
+  }
+
+  dynamic "tags" {
+    for_each = try(each.value.tags, [])
+    content {
+      key   = tags.value.key
+      value = tags.value.value
+    }
+  }
+
+  organization {
+    object_type = "organization.Organization"
+    moid        = local.org_moids[each.value.org_name]
+  }
+}

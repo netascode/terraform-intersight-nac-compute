c980853c15bcb287016cd946687993ed6988cf0c
Morten Skriver <moskrive@cisco.com>
2026-08-21 09:22:32 +0200

    make scsi boot IP address a toplevel argument

--- intersight_iscsi_boot_policies.tf
+++ intersight_iscsi_boot_policies.tf
@@ -3,19 +3,20 @@ locals {
     for org in local.filtered_intersight_organizations : [
       for policy in try(org.policies.iscsi_boot, []) :
       try(policy.managed, true) ? [{
-        key                           = format("%s/%s", org.name, policy.name)
-        org_name                      = org.name
-        name                          = policy.name
-        description                   = try(policy.description, local.defaults.compute.intersight.organizations.policies.iscsi_boot.description, "")
-        target_source_type            = try(policy.target_source_type, local.defaults.compute.intersight.organizations.policies.iscsi_boot.target_source_type)
-        initiator_ip_source           = try(policy.initiator_ip_source, local.defaults.compute.intersight.organizations.policies.iscsi_boot.initiator_ip_source)
-        auto_targetvendor_name        = try(policy.auto_targetvendor_name, local.defaults.compute.intersight.organizations.policies.iscsi_boot.auto_targetvendor_name)
-        ip_pool_key                   = try(policy.ip_pool, null) != null ? format("%s/%s", org.name, policy.ip_pool) : null
-        iscsi_adapter_policy_key      = try(policy.iscsi_adapter_policy, null) != null ? format("%s/%s", org.name, policy.iscsi_adapter_policy) : null
-        chap                          = try(policy.chap, null)
-        mutual_chap                   = try(policy.mutual_chap, null)
-        initiator_static_ip_v4_config = try(policy.initiator_static_ip_v4_config, null)
-        tags                          = try(policy.tags, [])
+        key                            = format("%s/%s", org.name, policy.name)
+        org_name                       = org.name
+        name                           = policy.name
+        description                    = try(policy.description, local.defaults.compute.intersight.organizations.policies.iscsi_boot.description, "")
+        target_source_type             = try(policy.target_source_type, local.defaults.compute.intersight.organizations.policies.iscsi_boot.target_source_type)
+        initiator_ip_source            = try(policy.initiator_ip_source, local.defaults.compute.intersight.organizations.policies.iscsi_boot.initiator_ip_source)
+        initiator_static_ip_v4_address = try(policy.initiator_static_ip_v4_address, null)
+        auto_targetvendor_name         = try(policy.auto_targetvendor_name, local.defaults.compute.intersight.organizations.policies.iscsi_boot.auto_targetvendor_name)
+        ip_pool_key                    = try(policy.ip_pool, null) != null ? format("%s/%s", org.name, policy.ip_pool) : null
+        iscsi_adapter_policy_key       = try(policy.iscsi_adapter_policy, null) != null ? format("%s/%s", org.name, policy.iscsi_adapter_policy) : null
+        chap                           = try(policy.chap, null)
+        mutual_chap                    = try(policy.mutual_chap, null)
+        initiator_static_ip_v4_config  = try(policy.initiator_static_ip_v4_config, null)
+        tags                           = try(policy.tags, [])
       }] : []
     ]
   ])
@@ -24,11 +25,12 @@ locals {
 resource "intersight_vnic_iscsi_boot_policy" "iscsi_boot_policy" {
   for_each = { for p in local.iscsi_boot_policies : p.key => p if var.manage_intersight_policies }
 
-  description            = each.value.description
-  name                   = each.value.name
-  target_source_type     = each.value.target_source_type
-  initiator_ip_source    = each.value.initiator_ip_source
-  auto_targetvendor_name = each.value.auto_targetvendor_name
+  description                    = each.value.description
+  name                           = each.value.name
+  target_source_type             = each.value.target_source_type
+  initiator_ip_source            = each.value.initiator_ip_source
+  initiator_static_ip_v4_address = each.value.initiator_static_ip_v4_address
+  auto_targetvendor_name         = each.value.auto_targetvendor_name
 
   dynamic "initiator_ip_pool" {
     for_each = each.value.ip_pool_key != null ? [1] : []
@@ -68,7 +70,6 @@ resource "intersight_vnic_iscsi_boot_policy" "iscsi_boot_policy" {
     for_each = each.value.initiator_static_ip_v4_config != null ? [each.value.initiator_static_ip_v4_config] : []
     content {
       object_type   = "comm.IpV4Interface"
-      ip_address    = try(initiator_static_ip_v4_config.value.ip_address, null)
       gateway       = try(initiator_static_ip_v4_config.value.default_gateway, null)
       netmask       = try(initiator_static_ip_v4_config.value.subnet_mask, null)
       primary_dns   = try(initiator_static_ip_v4_config.value.primary_dns, null)

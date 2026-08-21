74a0d0ca25183b97af37c998eb5583bd1c3d2821
Morten Skriver <moskrive@cisco.com>
2026-08-21 09:08:54 +0200

    map default_gateway/subnet_mask from the data model to gateway/netmask used by the provider

--- intersight_iscsi_boot_policies.tf
+++ intersight_iscsi_boot_policies.tf
@@ -67,11 +67,12 @@ resource "intersight_vnic_iscsi_boot_policy" "iscsi_boot_policy" {
   dynamic "initiator_static_ip_v4_config" {
     for_each = each.value.initiator_static_ip_v4_config != null ? [each.value.initiator_static_ip_v4_config] : []
     content {
-      object_type     = "comm.IpV4Interface"
-      default_gateway = try(initiator_static_ip_v4_config.value.default_gateway, null)
-      primary_dns     = try(initiator_static_ip_v4_config.value.primary_dns, null)
-      secondary_dns   = try(initiator_static_ip_v4_config.value.secondary_dns, null)
-      subnet_mask     = try(initiator_static_ip_v4_config.value.subnet_mask, null)
+      object_type   = "comm.IpV4Interface"
+      ip_address    = try(initiator_static_ip_v4_config.value.ip_address, null)
+      gateway       = try(initiator_static_ip_v4_config.value.default_gateway, null)
+      netmask       = try(initiator_static_ip_v4_config.value.subnet_mask, null)
+      primary_dns   = try(initiator_static_ip_v4_config.value.primary_dns, null)
+      secondary_dns = try(initiator_static_ip_v4_config.value.secondary_dns, null)
     }
   }
 
